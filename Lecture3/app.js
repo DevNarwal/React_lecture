fa3e2e417af0d2277412906143f91bee0c4a9983
DevNarwal <narwaldev029@gmail.com>
2025-08-22 01:25:32 +0530

    using js expression

--- Lecture3/app.js
+++ Lecture3/app.js
@@ -23,9 +23,25 @@ const new_element3=(
         <p>This  is a example of empty container that hold two element</p>
     </>
 )
+//**************************JSX (Java Script Expression)************************************/
+
+const name="Jaat";
+const obj={
+    khana:"dud dhai",
+    excercise:"sapate"
+}
+const style_ka_object={
+    color:"pink",
+    backgroundColor: "beige"
+}
+function meet(){
+    return <p>Shree ram janki baithe hai mere sine  m </p>
+}
+const new_element4=<h1 style={style_ka_object}>My self {name} Your Papa and i like {obj.khana} and to do {obj.excercise} JAI SHREE RAM!!!!!!!!! {meet()}</h1>
 
 const rootreact=ReactDOM.createRoot(document.getElementById('root'));
 rootreact.render(element);
 rootreact.render(new_element);
 rootreact.render(new_element2);
 rootreact.render(new_element3);
+rootreact.render(new_element4);
